e900df14b015d3b96ce45e3d4e33467c84d38806
lugit134 <lugitclass@gmail.com>
2026-05-18 11:05:24 -0400

    init

---
+++ src/main/java/cl/GestionDrones/v1/aeronaves/AeronavesApplication.java
@@ -0,0 +1,13 @@
+package cl.GestionDrones.v1.aeronaves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeronavesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeronavesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/GestionDrones/v1/aeronaves/AeronavesApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.GestionDrones.v1.aeronaves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeronavesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
